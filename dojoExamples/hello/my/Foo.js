e9b74fb76e9c055e02a234e446216c24f42a60c0
Fatih <bounfat@gmail.com>
2016-08-08 08:06:21 +0300

    added person.html

---
+++ dojoExamples/hello/my/Foo.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Fatih on 7.08.2016.
+ */
+
+define(["dojo/_base/declare", "my/Foo"], function(declare, Foo){
+    return declare(null, {
+        arr: [ 1, 2, 3, 4 ], // object. shared by all instances!
+        num: 5,              // non-object. not shared.
+        str: "string",       // non-object. not shared.
+        //obj: new Foo(),      // object. shared by all instances!
+
+        constructor: function(){
+            this.arr = [ 1, 2, 3]; // per-instance object.
+            //this.obj = new Foo();      // per-instance object.
+        }
+    });
+});
+
