0cda31d3915f4e55a30973e37b8f8ed810f0e025
Fatih <bounfat@gmail.com>
2016-08-09 17:49:27 +0300

    running

--- dojoExamples/homework/uni/UniTextBox.js
+++ dojoExamples/homework/uni/UniTextBox.js
@@ -2,17 +2,24 @@
  * Created by Fatih on 8.08.2016.
  */
 
-define(["dojo/_base/declare", "dijit/form/TextBox", "dojo/on",
-    "dojo/mouse"], function (declare, TextBox) {
+define([
+    "dojo/_base/declare",
+    "dijit/form/TextBox",
+    "dojo/on",
+    "dojo/mouse",
+    "dojo/dom"
+], function (declare, TextBox, on, mouse, dom) {
 
     var UniTextBox = declare("UniTextBox", TextBox, {
         constructor: function () {
             console.debug("oluştu");
         },
         value: "Uni TextBox",
-        onClick: function (evt) {
-            console.log("i was clicked");
+        postCreate: function () {
             this.inherited(arguments);
+            on(dom.byId("textbox"), "click", function(){
+                console.log("clicked");
+            });
         }
 
     });
