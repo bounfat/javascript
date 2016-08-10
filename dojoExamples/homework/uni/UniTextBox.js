d3339788fdf1c41fda11bc7ed7736dac5e70d0d5
Fatih <bounfat@gmail.com>
2016-08-10 12:22:37 +0300

    calisti

--- dojoExamples/homework/uni/UniTextBox.js
+++ dojoExamples/homework/uni/UniTextBox.js
@@ -4,23 +4,43 @@
 
 define([
     "dojo/_base/declare",
-    "dijit/form/TextBox",
+    "dijit/form/ValidationTextBox",
+    "dijit/_WidgetBase",
+    "dijit/_TemplatedMixin",
     "dojo/on",
-    "dojo/mouse",
+    "dojo/_base/lang",
     "dojo/dom"
-], function (declare, TextBox, on, mouse, dom) {
 
-    var UniTextBox = declare("UniTextBox", TextBox, {
+], function (declare, ValidationTextBox, _WidgetBase, _TemplatedMixin, on, lang, dom) {
+
+    var UniTextBox = declare("UniTextBox", [ValidationTextBox], {
         constructor: function () {
             console.debug("oluştu");
         },
-        value: "Uni TextBox",
+        value: null,
         postCreate: function () {
+
             this.inherited(arguments);
-            on(dom.byId("textbox"), "click", function(){
+            console.log("Post create");
+            on(dom.byId("uniTextBox"), "click", function () {
                 console.log("clicked");
             });
+        },
+
+        // Template kısmına tekrar bakılacak..!
+
+        /*templateString: "<div>" +
+         "<input " +
+         "data-dojo-attach-point='focusNode' " +
+         "data-dojo-attach-event='onclick: increment'" + " />"
+         + "</div>",*/
+
+        _setValueAttr: {node: "focusNode", type: "attribute", attribute: "value"},
+
+        increment: function () {
+            this.focusNode.value = ++this.value;
         }
+
 
     });
     return UniTextBox;
